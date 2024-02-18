620f1d89ad035f1d01b49b768f03c8d0ee0a5a77
Miriam Rief <miriam.r.rief@gmail.com>
2024-02-18 23:31:43 +0100

    major changes

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-<script>
-    'use strict';
-</script>
+var taeterAuswahl = null;
+function auswahl() {
+    var buttons = document.getElementsByName("choice");
+
+    buttons.forEach(function (button) {
+        if (button.checked) {
+            taeterAuswahl = button.value;
+            return;
+        }
+    })
+
+    var suspects = ["2", "6"];
+
+    if (suspects.includes(taeterAuswahl)) {
+        var element = document.getElementById(taeterAuswahl);
+        element.style.display = "block";
+    } else {
+        window.location = "failure.html";
+    }
+}
+
+function auswahl2() {
+    var buttons = document.getElementsByName("choice");
+
+    buttons.forEach(function (button) {
+        if (button.checked) {
+            taeterAuswahl = button.value;
+            return;
+        }
+    })
+
+    var suspects = ["8", "11"];
+
+    if (suspects.includes(taeterAuswahl)) {
+        window.location = "success.html";
+    } else {
+        window.location = "failure.html";
+    }
+}
